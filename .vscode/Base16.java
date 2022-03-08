db7018a059cf1b91a7150c3ac59f5816e3c997c7
jesusgpa <jesus.parrado@urjc.es>
2022-03-08 18:30:37 +0100

    Strings

---
+++ .vscode/Base16.java
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Base16 {
+
+    public static final int FirstChar = 'A';
+    public static final int LastChar = 'Z';
+
+    public static void main(String[] args) {
+
+        StdOut.println(FirstChar);
+        
+    }
+
+    public static String encode(byte[] content){
+        String encode = "XXX";
+
+        return encode;
+    }
+
+    public static byte[] decode(String text){
+        byte[] decode = new byte[0];
+
+        return decode;
+    }
+
+}
--- Cadenas.java
+++ Cadenas.java
@@ -17,5 +17,25 @@ public class Cadenas {
         String t = s + " que feo eres.";
 
         StdOut.println(t);
+
+        StdOut.println(isPalindrome("eres"));
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        int n = s.length();
+
+        for (int i = 0; i < n/2; i++)
+        {
+            if (s.charAt(i) != s.charAt(n-1-i))
+            {
+                return false;
+            }
+        }    
+        
+        return true;
+
+        
+
     }
 }
