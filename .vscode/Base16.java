7194eea47585bfb5b9a55f0fdfe5914195450672
jesusgpa <jesus.parrado@urjc.es>
2022-03-10 12:26:26 +0100

    operaciones bit

--- .vscode/Base16.java
+++ .vscode/Base16.java
@@ -8,6 +8,16 @@ public class Base16 {
     public static void main(String[] args) {
 
         StdOut.println(FirstChar);
+
+        byte b;
+        int i;
+        i = 2452345;
+        b = (byte)(i & 0xff); //me quedo con el byte menos significativo del entero
+        // b = 121 en binario 01111001
+        b = (byte) ((0x0f) & (i >> 4)); //me quedo con los bits 4 5 6 y 7
+        // b = 7 en binario 0111
+
+        StdOut.println(b);
         
     }
 
