392c24d64d86b3c9a3c3073a9214fad920a1c44d
Jesús Parrado Alameda <jesus.parrado@urjc.es>
2022-03-14 19:48:30 +0100

    identado

--- Whitelist.java
+++ Whitelist.java
@@ -1,24 +1,21 @@
 import edu.princeton.cs.algs4.*;
 
-public class Whitelist {
+public class Whitelist
+{
+    public static void main(String[] args)
+    {
+	In in = new In(args[0]);
+	int[] a = in.readAllInts();
+	StaticSETofInts set = new StaticSETofInts(a);
+	while (!StdIn.isEmpty()) {
+	    // Leer key
+	    int key = StdIn.readInt();
 
-    public static void main(String[] args) {
-
-        In in = new In(args[0]);
-        int[] a = in.readAllInts();
-        StaticSETofInts set = new StaticSETofInts(a); 
-        
-        while (!StdIn.isEmpty()) {
-
-            // Leer key
-            int key = StdIn.readInt();
-
-            // Mostrar key si no está en la lista blanca
-            if (!set.contains(key)) { 
-                StdOut.println(key);
-
-            } 
-        }
-    } 
-
+	    // Mostrar key si no está en la lista blanca	    
+	    if (!set.contains(key)) {
+		StdOut.println(key);
+	    }
+	}
+    }
 }
+    
