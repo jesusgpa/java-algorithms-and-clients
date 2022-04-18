430cc2276a074b439da70971f218a344edad1d42
Jesús Parrado Alameda <jesus.parrado@urjc.es>
2022-04-18 12:58:08 +0200

    ejemplos

---
+++ ThreeSum.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.*;
+
+public class ThreeSum {
+    
+    public static int count(int[] a) {
+    
+        int n = a.length;
+        int count = 0;
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    } 
+
+    public static void main(String[] args)  { 
+        
+        In in = new In(args[0]);
+        
+        int[] a = in.readAllInts();
+
+        StdOut.println(count(a));
+    } 
+} 
+
